7b9baa2cfbb4a57cefd035b04c3b7229a92a3243
Fernando Estevez <fernando.estevez@patagoniansys.com>
2024-08-13 15:45:57 -0300

    Added community page

---
+++ src/app/(home)/community/page.tsx
@@ -0,0 +1,26 @@
+import { Box, Typography, Stack } from "@mui/material";
+
+export default function Community() {
+  return (
+    <Box>
+      <Stack
+        minHeight={257}
+        display="flex"
+        justifyContent="center"
+        alignItems="flex-start"
+        color="white"
+        fontSize={40}
+        fontWeight="bold"
+        bgcolor={"#234F6A"}
+        px={20}
+      >
+        <Typography fontWeight={700} fontSize="40px">
+          Comunidad
+        </Typography>
+        <Typography fontWeight={400} fontSize="28px">
+          Construyendo juntos un futuro transparente
+        </Typography>
+      </Stack>
+    </Box>
+  );
+}
