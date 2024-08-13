8fc66c178420c2c5915c1b01d9d5154837574f5a
Fernando Estevez <fernando.estevez@patagoniansys.com>
2024-08-13 14:51:35 -0300

    Added home navbar and layout component

---
+++ src/components/Home/Navbar.tsx
@@ -0,0 +1,40 @@
+import NextLink from "next/link";
+import { AppBar, Toolbar, Box, Button, Typography, Link } from "@mui/material";
+
+function NavBar() {
+  return (
+    <AppBar position="static" color="default">
+      <Toolbar>
+        <Box display="flex" flexGrow={1}>
+          <Link href="/" component={NextLink}>
+            <img
+              src="/assets/mapa-inversiones-header.svg"
+              alt="Mapa Inversiones"
+            />
+          </Link>
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="flex-end"
+          alignItems="center"
+          gap={2.5}
+        >
+          <Link href="/about-us" component={NextLink}>
+            <Typography>Nosotros</Typography>
+          </Link>
+          <Button color="inherit">
+            <Typography>Comparación Internacional</Typography>
+          </Button>
+          <Link href="/community" component={NextLink}>
+            <Typography>Comunidad</Typography>
+          </Link>
+          <Link href="/community" component={NextLink}>
+            <Typography>Canales de ayuda</Typography>
+          </Link>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default NavBar;
